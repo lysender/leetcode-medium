7d61933ce97cde72c70dca190ae49a9d98578f0a
Lysender <lysender@darkstar-win>
2024-03-21 11:33:54 +0800

    fix: container with most water solution

--- src/bin/b20240321.rs
+++ src/bin/b20240321.rs
@@ -16,51 +16,20 @@ impl Solution {
             l_val.min(r_val) * (right as i32 - left as i32)
         }
 
-        let mut area: i32 = 0;
-
-        for x in 0..(height.len() - 1) {
-            for y in 1..height.len() {
-                let new_area = compute_area(&height, x, y);
-                if new_area > area {
-                    area = new_area;
-                }
-            }
-        }
-
-        area
-    }
-
-    pub fn max_area_op(height: Vec<i32>) -> i32 {
-        if height.len() < 2 {
-            return 0;
-        }
-
-        fn compute_area(config: &Vec<i32>, left: usize, right: usize) -> i32 {
-            let l_val = config[left];
-            let r_val = config[right];
-            l_val.min(r_val) * (right as i32 - left as i32)
-        }
-
         let mut l: usize = 0;
         let mut r: usize = height.len() - 1;
-        let mut max_l: usize = l;
-        let mut max_r: usize = r;
         let mut area: i32 = compute_area(&height, l, r);
 
         while l < r {
-            // Move left forward
-            l += 1;
-            let mut new_area = compute_area(&height, l, max_r);
-            if new_area > area {
-                max_l = l;
-                area = new_area;
+            // Whichever is lower must move inward
+            if height[l] < height[r] {
+                l += 1;
+            } else {
+                r -= 1;
             }
 
-            // Move right backward
-            r -= 1;
-            new_area = compute_area(&height, max_l, r);
+            let new_area = compute_area(&height, l, r);
             if new_area > area {
-                max_r = r;
                 area = new_area;
             }
         }
